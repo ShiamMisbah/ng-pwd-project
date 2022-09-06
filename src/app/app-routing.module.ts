be67b7e2a77b917f2694dd037f3b369dd9f1b5a6
Shiam Misbah <shiam.misbah42@gmail.com>
2022-09-06 14:43:04 +0600

    routing completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChartTableComponent } from './chart-table/chart-table.component';
+import { LineChartComponent } from './line-chart/line-chart.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'line-chart', component: LineChartComponent},
+  {path: 'column-chart', component: ChartTableComponent},
+  {path: 'progress-bar', component: ProgressBarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [LineChartComponent, ChartTableComponent, ProgressBarComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgChartsModule } from 'ng2-charts';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChartTableComponent } from './chart-table/chart-table.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
-import { ProgressBarComponent } from './progress-bar/progress-bar.component';
-import { LineChartComponent } from './line-chart/line-chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChartTableComponent,
-    ProgressBarComponent,
-    LineChartComponent
+    routingComponents,
   ],
   imports: [
     BrowserModule,
